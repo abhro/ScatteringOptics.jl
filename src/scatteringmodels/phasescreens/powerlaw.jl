2f645acd3561c61f85de150cdcaf09d75ad099a9
Abhro <5664668+abhro@users.noreply.github.com>
2025-05-15 16:43:16 -0400

    Fix use of DocStringExtensions in docstring

--- src/scatteringmodels/phasescreens/powerlaw.jl
+++ src/scatteringmodels/phasescreens/powerlaw.jl
@@ -1,10 +1,10 @@
 export PhaseScreenPowerLaw
-@doc raw"""
+"""
     $(TYPEDEF)
 
-Power spectrum model of ISM fluctuations, for use in generating a RefractivePhaseScreen object. 
-Requires an AbstractScatteringModel for scattering parameters as well as image x and y pixel sizes. Optional 
-input of velocity in x and y direction for moving phase screen.
+Power spectrum model of ISM fluctuations, for use in generating a RefractivePhaseScreen object.
+Requires an AbstractScatteringModel for scattering parameters as well as image x and y pixel sizes.
+Optional input of velocity in x and y direction for moving phase screen.
 
 # Fields
 $(FIELDS)
